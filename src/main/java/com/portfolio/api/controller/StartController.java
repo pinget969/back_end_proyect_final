c42109bd5480cf027dbaa2a6660373b403536a10
Nelson <njpinget@gmail.com>
2023-01-14 23:57:04 -0300

    api.controller StartController se modificó el return

--- src/main/java/com/portfolio/api/controller/StartController.java
+++ src/main/java/com/portfolio/api/controller/StartController.java
@@ -1,21 +1,16 @@
 package com.portfolio.api.controller;
 
-import java.util.Optional;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "https://nelson-pinget.web.app", maxAge = 3600)
 @RestController
-@RequestMapping("/")
 public class StartController {
 
-	@GetMapping
-	public ResponseEntity<?> read(@PathVariable Long id){
-		return ResponseEntity.ok("Bienvenido a la api rest realizada para Argentina Programa! Para ver la aplicación completa ingrese en: https://nelson-pinget.web.app");
+	@GetMapping("/")
+	public String start(@PathVariable Long id){
+		return "Bienvenido a la API Rest realizada para Argentina Programa! Para ver la aplicación completa ingrese en: https://nelson-pinget.web.app";
 	}
 }
